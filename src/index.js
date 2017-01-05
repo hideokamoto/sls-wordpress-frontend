d5947f6eb5b21148646208da5a898326703d63fa
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-06 00:51:26 +0900

    some update
    - add single post endpoint
    - some code allowed ESlint

--- src/index.js
+++ src/index.js
@@ -1,79 +1,63 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {renderToString} from 'react-dom/server'
 const WPAPI = require('wpapi')
 
-class App extends Component {
-  render() {
+const App = (props) => {
+  return (
+    <div id="container">
+      <LambdaInfo context={props.context} />
+      <WPPosts posts={props.posts} />
+    </div>
+  )
+}
+
+const LambdaInfo = (props) => {
+    const context = props.context
     return (
-      <div id="container">
-        <LambdaInfo context={this.props.context} />
-        <WPPosts posts={this.props.posts} />
-      </div>
+        <header>
+            <h1>Lambda Info</h1>
+            <table>
+                <tr><th>invokedFunctionArn</th><td>{context.invokedFunctionArn}</td></tr>
+                <tr><th>logGroupName</th><td>{context.logGroupName}</td></tr>
+            </table>
+        </header>
     )
-  }
 }
 
-class LambdaInfo extends Component {
-    constructor(props, context){
-        super(props)
-    }
-    render() {
-        const context = this.props.context
-        return (
-            <header>
-                <h1>Lambda Info</h1>
-                <table>
-                    <tr><th>invokedFunctionArn</th><td>{context.invokedFunctionArn}</td></tr>
-                    <tr><th>logGroupName</th><td>{context.logGroupName}</td></tr>
-                </table>
-            </header>
-        )
-    }
+const WPPost = (props) => {
+    const post = props.post
+    return (
+        <archive key={post.id}>
+            <h2>{post.title.rendered}</h2>
+            <div dangerouslySetInnerHTML={{__html: post.excerpt.rendered}} />
+        </archive>
+    )
 }
 
-class WPPost extends Component {
-    constructor(props, context) {
-        super(props)
-    }
-    render() {
-        const post = this.props.post
-        return (
-            <archive key={post.id}>
-                <h2>{post.title.rendered}</h2>
-                <div dangerouslySetInnerHTML={{__html: post.excerpt.rendered}} />
-            </archive>
-        )
-    }
-}
-
-class WPPosts extends Component {
-    constructor(props, context) {
-        super(props)
-    }
-    render() {
-        const postList = this.props.posts.map( post => {
-            return <WPPost post={post} />
-        })
-        return (
-            <main>
-                <h1>WP Postlist</h1>
-                {postList}
-            </main>
-        )
-    }
+const WPPosts = (props) => {
+    const postList = props.posts.map( post => {
+        return <WPPost post={post} />
+    })
+    return (
+        <main>
+            <h1>WP Postlist</h1>
+            {postList}
+        </main>
+    )
 }
 
 function safeStringify(obj) {
-  return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--')
+  return JSON.stringify(obj)
+    .replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--')
 }
 
 function renderFullPage(renderedContent, context) {
   context = safeStringify(context)
+
   return `<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500' rel='stylesheet' type='text/css'>
     </head>
     <body>
         ${renderedContent}
@@ -101,5 +85,33 @@ module.exports.index = (event, context, callback) => {
   })
   .catch((err) => {
     callback(err)
+  })
+}
+
+
+module.exports.post = (event, context, callback) => {
+  if ( event.path.slug === 'undefined' ) {
+      callback(new Error('[404] Not found'))
+  }
+  const wp = new WPAPI({'endpoint': 'https://api.wp-app.org/wp-json'})
+
+  wp.posts().slug(event.path.slug).then((data) => {
+      if ( data.length < 1) {
+          throw new Error(`'Post ${event.path.slug} not found'`)
+      }
+    const renderedContent = renderToString(
+        <App
+            context={context}
+            posts={data}
+        />
+    )
+    const renderedPage = renderFullPage(renderedContent, event)
+
+    callback(null, renderedPage)
+  })
+  .catch((err) => {
+      const renderedPage = renderFullPage('error', err)
+
+      callback(null,renderedPage)
   })
 }
