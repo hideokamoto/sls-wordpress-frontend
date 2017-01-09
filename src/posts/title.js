0d2205e6a7d044b6a5c65f3ee7e25fb7a70e2094
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-09 18:48:06 +0900

    allow eslint

--- src/posts/title.js
+++ src/posts/title.js
@@ -1,11 +1,14 @@
-const Mikan = require('../lib/mikan')
 import React from 'react'
 
+const Mikan = require('../lib/mikan')
 const PostTitle = (props) => {
   const title = Mikan(props.children)
 
   return (
-      <h2 className="post-title" dangerouslySetInnerHTML={{__html :title}} />
+      <h2
+          className="post-title"
+          dangerouslySetInnerHTML={{'__html': title}}
+      />
   )
 }
 
